5cde5c7bb22fbf4e702eb796188b8e9047c8c028
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-06-20 20:19:59 +0000

    adds users#show user#index

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,19 @@
+class PhotosController < ApplicationController 
+
+
+  def index
+
+
+    render({:template => "photos_template/index"})
+  end
+
+  def show
+  
+
+    render({:template => "photos_template/index"})
+  end
+
+
+
+
+end
