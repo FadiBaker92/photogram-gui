4ccc259bc9b870f12fd82e74e0a0dae6930e4305
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-06-22 03:39:26 +0000

    adds photos # delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,10 +9,20 @@ class PhotosController < ApplicationController
 
   def show
   
-
+    photo_id_url = params.fetch("path_photo_id")
+    matching_photos = Photo.where({:id => photo_id_url})
+    @the_photo = matching_photos.at(0)
     render({:template => "photos_templates/show"})
   end
 
+  def delete
+  
+    deleted_photo_url = params.fetch("deleted_photo_id")
+    matching_photos = Photo.where({:id => deleted_photo_url})
+    deleted_photo = matching_photos.at(0)
+    deleted_photo.destroy
+    redirect_to("/photos")
+  end
 
 
 
